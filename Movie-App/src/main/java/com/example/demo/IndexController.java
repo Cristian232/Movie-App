9de111ff6043f0176649751867d1ba3ee8fcf100
Cristian <39384968+Cristian232@users.noreply.github.com>
2019-08-19 01:52:33 +0300

    added json dep and runned a few checks

--- Movie-App/src/main/java/com/example/demo/IndexController.java
+++ Movie-App/src/main/java/com/example/demo/IndexController.java
@@ -2,15 +2,22 @@ package com.example.demo;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.boot.jackson.JsonObjectSerializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
@@ -38,14 +45,28 @@ public class IndexController {
 			URL url = new URL(reqbuilder.getStr());
 			HttpURLConnection con = (HttpURLConnection) url.openConnection();
 			con.setRequestMethod("GET");
-			
 			int status = con.getResponseCode();
 			
+			BufferedReader bufferedReader;
+			StringBuffer responseContent = new StringBuffer();
+			String line;
 			
-			for (int i = 0; i < con.getHeaderFields().size(); i++) {
-				System.out.println(con.getHeaderField(i) + " = " + i);
+			bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			while ((line = bufferedReader.readLine()) != null) {
+				responseContent.append(line);
 			}
-		 	
+			bufferedReader.close();
+			
+			System.out.println(responseContent );
+			
+			JSONArray jsonArray = new JSONArray(responseContent);
+			
+			JSONObject movie = new JSONObject(jsonArray.getJSONObject(3));
+			
+			for (int i = 0; i < movie.length(); i++) {
+				System.out.println();
+			}
+			
 	        return "result";
 	    }
 	
